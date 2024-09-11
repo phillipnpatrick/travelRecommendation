f4be7902140f0de98c14b6ec03c41b3b5d293f83
Phillip Patrick <phillip.n.patrick@gmail.com>
2024-09-11 16:43:47 -0400

    about us page complete

--- travel.js
+++ travel.js
@@ -36,6 +36,8 @@ function showAboutUs(show) {
     if (show) {
         document.getElementById("btnAbout").classList.add("active-page");
         document.getElementById("divAbout").style.display = "block";
+
+        getEmployees();
     } else {
         document.getElementById("btnAbout").classList.remove("active-page");
         document.getElementById("divAbout").style.display = "none";        
@@ -57,5 +59,36 @@ function showSearchBox(show) {
         document.getElementById("searchBox").style.display = "flex";
     } else {
         document.getElementById("searchBox").style.display = "none";
+    }
+}
+
+function getEmployees() {
+    const employeeCard = document.getElementById("employeeCards");
+
+    if (employeeCard.innerText === ""){
+        const url = window.location.href + "employees.json";
+        employeeCard.innerHTML = "";
+        let html = "";
+    
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                data.employees.forEach((employee) => {
+                    html += `<div class="employee-card-container">`;
+                    html += `<div class="employee-avatar">`;
+                    html += `<img src="${employee.photo}" alt="Employee Avatar" width="40" height="40"></img>`;
+                    html += `</div>`;
+                    html += `<div class="employee-card">`;
+                    html += `<span><strong>${employee.firstName} ${employee.lastName}</strong></span>`;
+                    html += `<p>${employee.shortBio}</p>`;
+                    html += `<button onclick="alert('${employee.bio}');">${employee.title}</button>`;
+                    html += `</div>`;
+                    html += `</div>`;
+                });
+                employeeCard.innerHTML = html;
+            })
+            .catch(error => {
+                console.error("Error: ", error);
+            });
     }
 }
