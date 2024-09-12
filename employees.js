affee5162cc1e1c47f36e401f8ef77b26fa30805
Phillip Patrick <phillip.n.patrick@gmail.com>
2024-09-11 20:57:29 -0600

    Home, About, and Contact done; top nav fixed

---
+++ employees.js
@@ -0,0 +1,31 @@
+const employees = [{
+    "employees" : [
+        {
+            "id": 100001,
+            "firstName": "Harry",
+            "lastName": "Potter",
+            "photo": "https://img.brickowl.com/files/image_cache/larger/lego-harry-potter-minifigure-975056-28.jpg",
+            "shortBio": "Harry is responsible for ...",
+            "bio": "Harry is responsible for strategy, operation, and financial goals.",
+            "title": "CEO"
+        },
+        {
+            "id": 100002,
+            "firstName": "Ron",
+            "lastName": "Weasley",
+            "photo": "https://img.brickowl.com/files/image_cache/larger/lego-ron-weasley-minifigure-937319-28.jpg",
+            "shortBio": "Ron is responsible for ...",
+            "bio": "Ron is responsible for guiding, monitoring and leading our travel team.",
+            "title": "Team Leader"
+        },
+        {
+            "id": 100003,
+            "firstName": "Hermione",
+            "lastName": "Granger",
+            "photo": "https://img.brickowl.com/files/image_cache/larger/lego-hermione-granger-minifigure-504803-28.jpg",
+            "shortBio": "Hermione is responsible for ...",
+            "bio": "Hermione is responsible for guiding and supervising our web projects.",
+            "title": "Project Leader"
+        }
+    ]
+}];
--- travel.js
+++ travel.js
@@ -57,7 +57,7 @@ function showContactUs(show) {
 
 function showSearchBox(show) {
     if (show) {
-        document.getElementById("searchBox").style.display = "flex";
+        document.getElementById("searchBox").style.display = "-webkit-inline-box";
     } else {
         document.getElementById("searchBox").style.display = "none";
     }
@@ -67,31 +67,41 @@ function getEmployees() {
     const employeeCard = document.getElementById("employeeCards");
 
     if (employeeCard.innerText === ""){
-        const url = window.location.href + "employees.json";
+        let url = window.location.href;
         employeeCard.innerHTML = "";
-        let html = "";
     
-        fetch(url)
-            .then(response => response.json())
-            .then(data => {
-                data.employees.forEach((employee) => {
-                    html += `<div class="employee-card-container">`;
-                    html += `<div class="employee-avatar">`;
-                    html += `<img src="${employee.photo}" alt="Employee Avatar" width="40" height="40"></img>`;
-                    html += `</div>`;
-                    html += `<div class="employee-card">`;
-                    html += `<span><strong>${employee.firstName} ${employee.lastName}</strong></span>`;
-                    html += `<p>${employee.shortBio}</p>`;
-                    html += `<button onclick="alert('${employee.bio}');">${employee.title}</button>`;
-                    html += `</div>`;
-                    html += `</div>`;
+        if (url.indexOf("github.io") > 0){
+            fetch(url)
+                .then(response => response.json())
+                .then(data => {
+                    employeeCard.innerHTML = getEmployeeCards(data.employees);
+                })
+                .catch(error => {
+                    console.error("Error: ", error);
                 });
-                employeeCard.innerHTML = html;
-            })
-            .catch(error => {
-                console.error("Error: ", error);
-            });
+        } else {
+            employeeCard.innerHTML = getEmployeeCards(employees[0].employees);
+        }
     }
+}
+
+function getEmployeeCards(employees) {
+    let html = "";
+
+    employees.forEach((employee) => {
+        html += `<div class="employee-card-container">`;
+        html += `<div class="employee-avatar">`;
+        html += `<img src="${employee.photo}" alt="Employee Avatar" width="40" height="40"></img>`;
+        html += `</div>`;
+        html += `<div class="employee-card">`;
+        html += `<span><strong>${employee.firstName} ${employee.lastName}</strong></span>`;
+        html += `<p>${employee.shortBio}</p>`;
+        html += `<button onclick="alert('${employee.bio}');">${employee.title}</button>`;
+        html += `</div>`;
+        html += `</div>`;
+    });
+
+    return html;
 }
 
 function sendEmail(event) {
